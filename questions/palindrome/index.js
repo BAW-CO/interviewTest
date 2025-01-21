2630ef2ee751c38313687a4ea37ee50895f4e76e
BAW-CO <bridgetw.designs@gmail.com>
2025-01-20 22:06:45 -0700

    palindromes

--- questions/palindrome/index.js
+++ questions/palindrome/index.js
@@ -9,8 +9,37 @@
  * palindrome('frontend') === false
  */
 
+// const palindrome = (str) => {
+//   const reversedString = str.split("").reverse().join("");
+//   return str === reversedString;
+// };
+
+
+// const palindrome = (str) => {
+//   return str === reversedString(str);
+// };
+// const reversedString = (str) => {
+//     return str.split("").reverse().join("");
+// }
+
+
+// const palindrome = (str) => {
+//     return str.split("").every((char, index) => (
+//         char === str[str.length - index - 1]
+//     ))
+// };
+
+
 const palindrome = (str) => {
-  
+    const middle = Math.floor(str.length / 2);
+    for (let i = 0; i < middle; ++i) {
+        const oppositeIndex = str.length -i - 1;
+        if (str[i] !== str[oppositeIndex]) {
+            return false;
+        }
+    }
+    return true;
 };
+
 
 module.exports = palindrome;
